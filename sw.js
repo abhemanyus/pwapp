9e296b1d07c67fb0e611d018bdcd1264a67c460d
Aaron Smith <abhemanyus@gmail.com>
2021-08-21 11:25:18 +0530

    app's offline, boys

--- sw.js
+++ sw.js
@@ -1,11 +1,39 @@
+const staticCacheName = 'site-static';
+const assets = [
+  './',
+  './index.html',
+  './pages/about.html',
+  './pages/contact.html',
+  './js/app.js',
+  './js/ui.js',
+  './js/materialize.js',
+  './css/materialize.css',
+  './css/style.css',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  './img/icons/favicon-196.png',
+  './img/icons/apple-icon-180.png',
+  './img/icons/manifest-icon-192.png',
+  'https://fonts.gstatic.com/s/materialicons/v98/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2',
+  './manifest.json'
+];
+
 self.addEventListener('install', evt => {
-  console.log('Service worker has been installed.');
+  evt.waitUntil(
+    caches.open(staticCacheName)
+    .then(cache => {
+      console.log("pre-caching assets");
+      cache.addAll(assets);
+    })
+    .catch(err => {console.log('Failed to open cache', err)})
+  );
 });
 
 self.addEventListener('activate', evt => {
-  console.log('Service worker has been activated');
+
 });
 
 self.addEventListener('fetch', evt => {
-  console.log('Fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request)
+  );
 });
